676cc754c1a938f89ee8fc6597ddf45de9b1c89e
LoTwT <709937065@qq.com>
2022-06-20 07:57:29 +0800

    feat: start server base procedures

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,4 +1,5 @@
 import cac from "cac"
+import { startDevServer } from "./server"
 
 const cli = cac()
 
@@ -9,7 +10,7 @@ cli
   .alias("serve")
   .alias("dev")
   .action(async () => {
-    console.log("测试 cli")
+    await startDevServer()
   })
 
 cli.help()
