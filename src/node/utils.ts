0629cc07fea253840bd101fe0be3e8686952798b
LoTwT <709937065@qq.com>
2022-06-28 15:31:45 +0800

    feat: path resolve plugin

--- src/node/utils.ts
+++ src/node/utils.ts
@@ -1,5 +1,6 @@
 import os from "node:os"
 import path from "node:path"
+import { HASH_RE, JS_TYPES_RE, QEURY_RE } from "./constants"
 
 export function slash(p: string): string {
   return p.replace(/\\/g, "/")
@@ -9,3 +10,16 @@ export const isWindows = os.platform() === "win32"
 export function normalizePath(id: string): string {
   return path.posix.normalize(isWindows ? slash(id) : id)
 }
+
+export const isJSRequest = (id: string): boolean => {
+  id = cleanUrl(id)
+
+  if (JS_TYPES_RE.test(id)) return true
+
+  if (!path.extname(id) && !id.endsWith("/")) return true
+
+  return false
+}
+
+export const cleanUrl = (url: string): string =>
+  url.replace(HASH_RE, "").replace(QEURY_RE, "")
