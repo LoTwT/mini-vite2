709579d336bc99c124e58cbc4109c4e7a49658ed
LoTwT <709937065@qq.com>
2022-08-27 22:53:07 +0800

    chore: reset repo

--- README.md
+++
@@ -1,5 +0,0 @@
-# mini-vite2
-
-## todo
-
-- [ ] [windows 依赖预构建时的路径处理](https://github.com/sanyuan0704/juejin-book-vite/issues/3)
--- src/node/plugins/index.ts
+++
@@ -1,8 +0,0 @@
-import { Plugin } from "../plugin"
-import { esbuildTransformPlugin } from "./esbuild"
-import { importAnalysisPlugin } from "./importAnalysis"
-import { resolvePlugin } from "./resolve"
-
-export function resolvePlugins(): Plugin[] {
-  return [resolvePlugin(), esbuildTransformPlugin(), importAnalysisPlugin()]
-}
