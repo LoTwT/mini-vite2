22b7165cae0121925e001849795b9a1137fed9d9
LoTwT <709937065@qq.com>
2022-09-05 21:14:34 +0800

    feat: cli and startDevServer

---
+++ src/node/server/index.ts
@@ -0,0 +1,19 @@
+// connect 是一个具有中间件机制的轻量级 Node.js 框架
+// 既可以单独作为服务器，也可以接入到任何具有中间件机制的框架中，如 Koa、Express
+import connect from "connect";
+// picocolors 是一个用来在命令行显示不同颜色文本的工具
+import { blue, green } from "picocolors";
+
+export async function startDevServer() {
+  const app = connect();
+  const root = process.cwd();
+  const startTime = Date.now();
+
+  app.listen(3000, async () => {
+    console.log(
+      green("🚀 No-Bundle 服务已经成功启动！"),
+      `耗时：${Date.now() - startTime}ms`
+    );
+    console.log(`> 本地访问路径：${blue("http://localhost:3000")}`);
+  });
+}
